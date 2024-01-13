9089c2f1ca24a9103da42ae6b70b1864b1e757df
Mateusz Skrobis <mateuszskrobis0707@gmail.com>
2024-01-13 04:15:24 +0100

    feat: task 2023.12.03 result

--- tasks/2023-12-03/index.ts
+++ tasks/2023-12-03/index.ts
@@ -1,32 +1,36 @@
 export interface Lokalizacja {
-    x: number;
-    y: number;
-    z: number;
-    czas: number;
+  x: number
+  y: number
+  z:number
+  czas: number
 }
 
-export type MapaCzasoprzestrzenna = (x: number, y: number, z: number, czas: number) => number;
-
-
+export type MapaCzasoprzestrzenna = (x: number, y: number, z: number, czas: number) => number
 
 export function znajdzWorek(lokalizacje: Lokalizacja[], mapa: MapaCzasoprzestrzenna): Lokalizacja | null {
-    if (lokalizacje.length === 0) {
-        return null;
+  if (lokalizacje.length === 0) {
+    return null
+  }
+
+  let biggestMap: Lokalizacja = lokalizacje[0]
+
+  for(let i = 0; i < lokalizacje.length; i++) {
+    const result = getMapValue(lokalizacje[i], mapa)
+
+    if(!result) {
+      return null
     }
 
-    let maxWartosc = -Infinity;
-    let najlepszaLokalizacja: Lokalizacja | null = null;
-
-    for (const lokalizacja of lokalizacje) {
-        const { x, y, z, czas } = lokalizacja;
-        const wartoscMapy = mapa(x, y, z, czas);
-
-        if (!isNaN(wartoscMapy) && isFinite(wartoscMapy) && wartoscMapy > maxWartosc) {
-            maxWartosc = wartoscMapy;
-            najlepszaLokalizacja = lokalizacja
-        }
+    if(result > getMapValue(biggestMap, mapa)) {
+       biggestMap = lokalizacje[i]
     }
+  }
 
-    return najlepszaLokalizacja;
+  return biggestMap
+}
 
+function getMapValue  (mapa: Lokalizacja, cb: MapaCzasoprzestrzenna) {
+  const { x, y, z, czas} = mapa
+
+  return cb(x, y, z, czas)
 }
